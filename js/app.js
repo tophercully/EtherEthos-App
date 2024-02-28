81feea77ba323041b15941d61ff13bc1b15ef1f0
tophercully <89924670+tophercully@users.noreply.github.com>
2024-02-28 17:14:26 -0600

    dynamic population for EOA, tags, badges, respects

--- js/app.js
+++ js/app.js
@@ -809,16 +809,313 @@ if (edit_btn && view_btn && module_view_arr && module_edit_arr) {
       toggleClasses(module_edit_arr[i], [], ["hidden", "lg:hidden"]);
     }
 
+    //autopopulate form info when available
     if(eeArray) {
       console.log('eeArray populated, prefilling form')
-      const editAlias = document.querySelectorAll('[data-edit-field="alias"]')
-      const editDetail = document.querySelectorAll('[data-edit-field="detail"]')
-      const editSocial = document.querySelectorAll('[data-edit-field="social"]')
-      const editWebsite = document.querySelectorAll('[data-edit-field="website"]')
-      const editGallery = document.querySelectorAll('[data-edit-field="gallery"]')
-
-      console.log(editAlias.value, editDetail.value)
+      //basic info
+      const editAlias = document.querySelectorAll('[data-edit-field="alias"]')[0]
+      const editDetail = document.querySelectorAll('[data-edit-field="detail"]')[0]
+      const editSocial = document.querySelectorAll('[data-edit-field="social"]')[0]
+      const editWebsite = document.querySelectorAll('[data-edit-field="website"]')[0]
+      const editGallery = document.querySelectorAll('[data-edit-field="gallery"]')[0]
+      const editPFPToken = document.querySelectorAll('[data-edit-field="pfp-id"]')[0]
+      const editPFPAddress = document.querySelectorAll('[data-edit-field="pfp-address"]')[0]
+      
       editAlias.value = eeArray[0][0]
+      editDetail.value = eeArray[0][1]
+      editSocial.value = eeArray[0][2]
+      editWebsite.value = eeArray[0][3]
+      editGallery.value = eeArray[0][4]
+      editPFPToken.value = eeArray[0][5]
+      editPFPAddress.value = eeArray[0][6]
+      
+      //verification field
+      const editVerification = document.querySelectorAll('[data-edit-field="user-verification"]')[0]
+      if(verificationResponse) {
+        editVerification.value = verificationResponse
+      }
+      
+      //tags info
+      //handled dynamically
+      const allTags = eeArray[6]
+      if(allTags) {
+        const tagsEditParent = document.querySelectorAll('[data-edit-field="tags"]')[0]
+        function createTagInput(tagsContainer, index, defaultInput) {
+          //create the li
+          var thisTag = document.createElement("li")
+          thisTag.setAttribute('class', 'my-2 flex items-center before:mr-4 before:inline-block before:h-2 before:w-2 before:rounded-full before:bg-main')
+          thisTag.setAttribute('data-edit-item', '')
+          //create input
+          var thisInput = document.createElement('input')
+          thisInput.setAttribute('type', 'text')
+          thisInput.setAttribute('placeholder', 'Tag Name')
+          thisInput.setAttribute('class', 'mr-4 max-h-10 rounded-md border border-main px-3 py-3 text-md lg:w-1/3')
+          thisInput.setAttribute('data-field-edit', 'tag')
+          thisTag.appendChild(thisInput)
+          //create write button
+          var thisWrite = document.createElement('button')
+          thisWrite.setAttribute('class', 'mx-2 h-7 w-7 rounded-full bg-main p-0')
+          thisWrite.setAttribute('data-write', '')
+          var writeImg = document.createElement('img')
+          writeImg.setAttribute('class', 'm-auto h-3/5 w-3/5 object-contain')
+          writeImg.setAttribute('src', './svg/write.svg')
+          writeImg.setAttribute('alt', 'Wallet Logo')
+          thisWrite.appendChild(writeImg)
+          thisTag.appendChild(thisWrite)
+          //create delete button
+          var thisDelete = document.createElement('button')
+          thisDelete.setAttribute('class', 'mx-2 h-7 w-7 rounded-full bg-main p-0')
+          thisDelete.setAttribute('data-delete', '')
+          var deleteImg = document.createElement('img')
+          deleteImg.setAttribute('class', 'm-auto h-3/5 w-3/5 object-contain')
+          deleteImg.setAttribute('src', './svg/delete.svg')
+          deleteImg.setAttribute('alt', 'Wallet Logo')
+          thisDelete.appendChild(deleteImg)
+          thisTag.appendChild(thisDelete)
+          //attach li to list
+
+          //prepopulate if available
+          console.log(tagsContainer)
+          tagsContainer.appendChild(thisTag)
+          if(defaultInput) {
+            thisInput.value = defaultInput
+          }
+
+          thisWrite.addEventListener('click', ()=> {
+            console.log('updating tag')
+            if(defaultInput) {
+              updateTagToContract(index, thisInput.value)
+            } else {
+              pushTagToContract(thisInput.value)
+            }
+          })
+        }
+        //populate all existing
+        for(let i = 0; i < allTags.length; i++) {
+          var tag = allTags[i]
+          createTagInput(tagsEditParent, i, tag)
+        }
+        //create an empty field for additional accounts
+        createTagInput(tagsEditParent)
+      }
+
+      //badges info
+      //handled dynamically
+      const allBadges = eeArray[7]
+      if(allBadges) {
+        const badgesEditParent = document.querySelectorAll('[data-edit-field="badges"]')[0]
+        function createTagInput(BadgesContainer, defaultName, defaultAddress) {
+          //create the li
+          var thisBadge = document.createElement("li")
+          thisBadge.setAttribute('class', 'my-2 flex items-center before:mr-4 before:inline-block before:h-2 before:w-2 before:rounded-full before:bg-main')
+          thisBadge.setAttribute('data-edit-item', '')
+          //create input1
+          var inputName = document.createElement('input')
+          inputName.setAttribute('type', 'text')
+          inputName.setAttribute('placeholder', 'Badge Name')
+          inputName.setAttribute('class', 'max-h-10 w-28 rounded-md border border-main px-3 py-3 text-md lg:w-1/3')
+          thisBadge.appendChild(inputName)
+          //create span with ':' divider
+          var thisBadgeSpan = document.createElement('span')
+          thisBadgeSpan.setAttribute('class', 'mx-2')
+          thisBadgeSpan.innerHTML = ':'
+          thisBadge.appendChild(thisBadgeSpan)
+          //create input1
+          var inputAddress = document.createElement('input')
+          inputAddress.setAttribute('type', 'text')
+          inputAddress.setAttribute('placeholder', 'Receiving Address')
+          inputAddress.setAttribute('class', 'mr-4 max-h-10 w-28 rounded-md border border-main px-3 py-3 text-md lg:w-1/3')
+          thisBadge.appendChild(inputAddress)
+          //create write button
+          var thisWrite = document.createElement('button')
+          thisWrite.setAttribute('class', 'mx-2 h-7 w-7 rounded-full bg-main p-0')
+          var writeImg = document.createElement('img')
+          writeImg.setAttribute('class', 'm-auto h-3/5 w-3/5 object-contain')
+          writeImg.setAttribute('src', './svg/write.svg')
+          writeImg.setAttribute('alt', 'Wallet Logo')
+          thisWrite.appendChild(writeImg)
+          thisBadge.appendChild(thisWrite)
+          //create delete button
+          var thisDelete = document.createElement('button')
+          thisDelete.setAttribute('class', 'mx-2 h-7 w-7 rounded-full bg-main p-0')
+          var deleteImg = document.createElement('img')
+          deleteImg.setAttribute('class', 'm-auto h-3/5 w-3/5 object-contain')
+          deleteImg.setAttribute('src', './svg/delete.svg')
+          deleteImg.setAttribute('alt', 'Wallet Logo')
+          thisDelete.appendChild(deleteImg)
+          thisBadge.appendChild(thisDelete)
+          //attach li to list
+          BadgesContainer.appendChild(thisBadge)
+          if(defaultAddress || defaultName) {
+            inputName.value = defaultName
+            inputAddress.value = defaultAddress
+          }
+        }
+        //populate all existing
+        for(let i = 0; i < allBadges.length; i++) {
+          var badgeAddress = allBadges[i]
+          var badgeDesc
+          createTagInput(badgesEditParent, badge)
+        }
+        //create an empty field for additional accounts
+        createTagInput(badgesEditParent)
+      }
+
+
+      //Associated accounts info
+      //handled dynamically
+      const EOAs = eeArray[2]
+      console.log(EOAs.length)
+      if(EOAs.length/2 > 0) {
+        const EOAEditParent = document.querySelectorAll('[data-edit-field="associated"]')[0]
+        function createEAOInput(EOAContainer, index, defaultDesc, defaultAddress) {
+          indexTuple = index * 2 //should target this address in the doubled array
+
+          //create the li
+          var thisEOA = document.createElement("li")
+          thisEOA.setAttribute('class', 'my-2 flex items-center before:mr-4 before:inline-block before:h-2 before:w-2 before:rounded-full before:bg-main')
+          thisEOA.setAttribute('data-edit-item', '')
+          //create input1
+          var inputDesc = document.createElement('input')
+          inputDesc.setAttribute('type', 'text')
+          inputDesc.setAttribute('placeholder', 'Description')
+          inputDesc.setAttribute('class', 'max-h-10 w-28 rounded-md border border-main px-3 py-3 text-md lg:w-1/3')
+          inputDesc.setAttribute('data-field-edit', 'associated-desc')
+          thisEOA.appendChild(inputDesc)
+          //create span with ':' divider
+          var thisEOASpan = document.createElement('span')
+          thisEOASpan.setAttribute('class', 'mx-2')
+          thisEOASpan.innerHTML = ':'
+          thisEOA.appendChild(thisEOASpan)
+          //create input1
+          var inputAddress = document.createElement('input')
+          inputAddress.setAttribute('type', 'text')
+          inputAddress.setAttribute('placeholder', 'Address')
+          inputAddress.setAttribute('class', 'mr-4 max-h-10 w-28 rounded-md border border-main px-3 py-3 text-md lg:w-1/3')
+          inputAddress.setAttribute('data-field-edit', 'associated-address')
+          thisEOA.appendChild(inputAddress)
+          //create write button
+          var thisWrite = document.createElement('button')
+          thisWrite.setAttribute('class', 'mx-2 h-7 w-7 rounded-full bg-main p-0')
+          thisWrite.setAttribute('data-write', '')
+          var writeImg = document.createElement('img')
+          writeImg.setAttribute('class', 'm-auto h-3/5 w-3/5 object-contain')
+          writeImg.setAttribute('src', './svg/write.svg')
+          writeImg.setAttribute('alt', 'Wallet Logo')
+          thisWrite.appendChild(writeImg)
+          thisEOA.appendChild(thisWrite)
+          //create delete button
+          var thisDelete = document.createElement('button')
+          thisDelete.setAttribute('class', 'mx-2 h-7 w-7 rounded-full bg-main p-0')
+          thisDelete.setAttribute('data-delete', '')
+          var deleteImg = document.createElement('img')
+          deleteImg.setAttribute('class', 'm-auto h-3/5 w-3/5 object-contain')
+          deleteImg.setAttribute('src', './svg/delete.svg')
+          deleteImg.setAttribute('alt', 'Wallet Logo')
+          thisDelete.appendChild(deleteImg)
+          thisEOA.appendChild(thisDelete)
+          //attach li to list
+          EOAContainer.appendChild(thisEOA)
+          if(defaultAddress || defaultDesc) {
+            inputDesc.value = defaultDesc
+            inputAddress.value = defaultAddress
+          }
+
+          thisWrite.addEventListener('click', () => {
+            console.log(inputDesc.value, ':', inputAddress.value, 'was written to index[', indexTuple, 'and', (indexTuple+1) + ']')
+            if(defaultAddress || defaultDesc) {
+              //exists, needs to update
+              console.log('updating '+ indexTuple)
+              updateEOAToContract(index, inputAddress.value, inputDesc.value)
+            } else {
+              //doesn't exist yet, needs a new entry
+              console.log('sent new entry to tuple index', indexTuple)
+              pushEOAToContract(inputAddress.value, inputDesc.value)
+            }
+          })
+          thisDelete.addEventListener('click', () => {
+            console.log('deleting index[', indexTuple, 'and', (indexTuple+1) + ']')
+            inputAddress.value = ' '
+            inputDesc.value = ' '
+            if(defaultAddress || defaultDesc) {
+              deleteEOAFromContract(index)
+            }
+          })
+        }
+        //populate all existing
+        for(let i = 0; i < EOAs.length/2; i++) {
+          var EOA = EOAs[i*2]
+          var EOADesc = EOAs[(i*2)+1]
+          createEAOInput(EOAEditParent, i, EOADesc, EOA)
+        }
+        //create an empty field for additional accounts
+        createEAOInput(EOAEditParent, EOAs.length)
+      }
+
+      //respect info
+      //handled dynamically
+      const allRespects = eeArray[6]
+      if(allRespects) {
+        const respectsEditParent = document.querySelectorAll('[data-edit-field="respect"]')[0]
+        console.log('container', respectsEditParent)
+        function createTagInput(respectsContainer, index, defaultInput) {
+          //create the li
+          var thisRespect = document.createElement("li")
+          thisRespect.setAttribute('class', 'my-2 flex items-center before:mr-4 before:inline-block before:h-2 before:w-2 before:rounded-full before:bg-main')
+          thisRespect.setAttribute('data-edit-item', '')
+          //create input
+          var thisInput = document.createElement('input')
+          thisInput.setAttribute('type', 'text')
+          thisInput.setAttribute('placeholder', 'Account to respect')
+          thisInput.setAttribute('class', 'mr-4 max-h-10 rounded-md border border-main px-3 py-3 text-md lg:w-1/3')
+          thisInput.setAttribute('data-field-edit', 'tag')
+          thisRespect.appendChild(thisInput)
+          //create write button
+          var thisWrite = document.createElement('button')
+          thisWrite.setAttribute('class', 'mx-2 h-7 w-7 rounded-full bg-main p-0')
+          thisWrite.setAttribute('data-write', '')
+          var writeImg = document.createElement('img')
+          writeImg.setAttribute('class', 'm-auto h-3/5 w-3/5 object-contain')
+          writeImg.setAttribute('src', './svg/write.svg')
+          writeImg.setAttribute('alt', 'Wallet Logo')
+          thisWrite.appendChild(writeImg)
+          thisRespect.appendChild(thisWrite)
+          //create delete button
+          var thisDelete = document.createElement('button')
+          thisDelete.setAttribute('class', 'mx-2 h-7 w-7 rounded-full bg-main p-0')
+          thisDelete.setAttribute('data-delete', '')
+          var deleteImg = document.createElement('img')
+          deleteImg.setAttribute('class', 'm-auto h-3/5 w-3/5 object-contain')
+          deleteImg.setAttribute('src', './svg/delete.svg')
+          deleteImg.setAttribute('alt', 'Wallet Logo')
+          thisDelete.appendChild(deleteImg)
+          thisRespect.appendChild(thisDelete)
+          //attach li to list
+
+          //prepopulate if available
+          console.log(respectsContainer)
+          respectsContainer.appendChild(thisRespect)
+          if(defaultInput) {
+            thisInput.value = defaultInput
+          }
+
+          thisWrite.addEventListener('click', ()=> {
+            console.log('updating tag')
+            if(defaultInput) {
+              updateTagToContract(index, thisInput.value)
+            } else {
+              pushTagToContract(thisInput.value)
+            }
+          })
+        }
+        //populate all existing
+        for(let i = 0; i < allRespects.length; i++) {
+          var respect = allRespects[i]
+          createTagInput(respectsEditParent, i, tag)
+        }
+        //create an empty field for additional accounts
+        createTagInput(respectsEditParent)
+      }
     } else {
       console.log('no eeArray, failed to prefill form')
     }
--- js/editFunc.js
+++ js/editFunc.js
@@ -28,14 +28,14 @@ elementsWithDataWrite.forEach(element => {
 });
 
 
-const pushFieldToContract = async (func, arguments) => {
+const pushSingleFieldToContract = async (func, argument) => {
     // let chainContract 
     // if(chain == sepolia){
         // chainContract = EE_Contract_Alchemy_Sepolia
     // } else {
         
     try {
-        await EE_Contract_Alchemy_Sepolia.methods.setAlias(currentAccount, 'chrisssssss').call(
+        await EE_Contract_User.methods[func](currentAccount, argument).send(
           {
             from: currentAccount
           },
@@ -53,7 +53,95 @@ const pushFieldToContract = async (func, arguments) => {
 
 const pushAliasToContract = async () => {
     try {
-        await EE_Contract_Alchemy_Sepolia.methods.setAlias(currentAccount, 'chrisssssss').call(
+        await EE_Contract_User.methods.setAlias(currentAccount, 'chrisssssss').send(
+          {
+            from: currentAccount
+          },
+          function (err, res) {
+            if (err) {
+              console.log(err);
+              return;
+            }
+          }
+        );
+    } catch (errorMessage) {
+        error = true;
+    }
+}
+
+const pushTagToContract = async (value) => {
+    try {
+        await EE_Contract_User.methods.pushTag(currentAccount, value).send(
+          {
+            from: currentAccount
+          },
+          function (err, res) {
+            if (err) {
+              console.log(err);
+              return;
+            }
+          }
+        );
+    } catch (errorMessage) {
+        error = true;
+    }
+}
+const updateTagToContract = async (index, value) => {
+    try {
+        await EE_Contract_User.methods.pushTag(currentAccount, index, value).send(
+          {
+            from: currentAccount
+          },
+          function (err, res) {
+            if (err) {
+              console.log(err);
+              return;
+            }
+          }
+        );
+    } catch (errorMessage) {
+        error = true;
+    }
+}
+const pushEOAToContract = async (address, desc) => {
+    try {
+        await EE_Contract_User.methods.pushAssociatedAccount(currentAccount, address, desc).send(
+          {
+            from: currentAccount
+          },
+          function (err, res) {
+            if (err) {
+              console.log(err);
+              return;
+            }
+          }
+        );
+    } catch (errorMessage) {
+        error = true;
+    }
+}
+const updateEOAToContract = async (index, address, desc) => {
+    try {
+        console.log('update function running')
+        await EE_Contract_User.methods.pushAssociatedAccount(currentAccount, index, address, desc).send(
+          {
+            from: currentAccount
+          },
+          function (err, res) {
+            if (err) {
+              console.log(err);
+              return;
+            }
+          }
+        );
+    } catch (errorMessage) {
+        error = true;
+    }
+}
+const deleteEOAFromContract = async (index) => {
+    
+    try {
+        await EE_Contract_User.methods.deleteAssociatedAccount(currentAccount, index).send(
           {
             from: currentAccount
           },
