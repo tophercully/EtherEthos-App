83f4e824f21a70362c7ccc57414e48fa401a2f43
tophercully <89924670+tophercully@users.noreply.github.com>
2024-03-24 16:28:07 -0500

    update env variable handling

--- webpack.config.dev.js
+++ webpack.config.dev.js
@@ -1,9 +1,20 @@
 const { merge } = require('webpack-merge');
 const common = require('./webpack.common.js');
+const webpack = require('webpack');
+const dotenv = require('dotenv');
+
+// const Dotenv = require('dotenv-webpack');
+const env = dotenv.config().parsed;
+
 
 module.exports = merge(common, {
   mode: 'development',
   devtool: 'inline-source-map',
+  plugins: [
+    new webpack.DefinePlugin({
+        'process.env': JSON.stringify(env)
+    })
+],
   devServer: {
     liveReload: true,
     hot: true,
